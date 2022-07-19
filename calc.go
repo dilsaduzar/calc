0aa43ec5bb6676810235edc121f00a427af7dfd0
dilsad <uzardilsad@gmail.com>
2022-07-19 14:25:15 +0300

    initial calc.go file

---
+++ calc.go
@@ -0,0 +1,15 @@
+package calc
+
+func Add(x, y int) int {
+	return x + y
+}
+func Sub(x, y int) int {
+	return x - y
+}
+func Mult(x, y int) int {
+	return x * y
+}
+func Div(x, y float64) float64 {
+	return x / y
+
+}
