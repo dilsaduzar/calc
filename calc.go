5538a72a4b7c24da6a3116822f646945203d9338
dilsad <uzardilsad@gmail.com>
2022-08-28 16:28:43 +0300

    revised calc.go file

--- calc.go
+++ calc.go
@@ -1,5 +1,9 @@
 package calc
 
+import (
+	"errors"
+)
+
 func Add(x, y float64) (float64, error) {
 	return x + y, nil
 }
@@ -12,5 +16,8 @@ func Mult(x, y float64) (float64, error) {
 	return x * y, nil
 }
 func Div(x, y float64) (float64, error) {
+	if y == 0 {
+		return 0, errors.New(`invalid value.dividing number can not be "0"`)
+	}
 	return x / y, nil
 }
