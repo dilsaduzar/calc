75f8f47a238150f9d929c371dab5b7509b659d68
dilsad <uzardilsad@gmail.com>
2022-08-28 15:48:15 +0300

    created calc.go file

---
+++ calc.go
@@ -0,0 +1,16 @@
+package calc
+
+func Add(x, y float64) (float64, error) {
+	return x + y, nil
+}
+
+func Sub(x, y float64) (float64, error) {
+	return x - y, nil
+}
+
+func Mult(x, y float64) (float64, error) {
+	return x * y, nil
+}
+func Div(x, y float64) (float64, error) {
+	return x / y, nil
+}
