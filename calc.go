8670c2ca5589af0bbed91435c1dce2ef6ec947a1
dilsad <uzardilsad@gmail.com>
2022-10-07 16:42:23 +0300

    corrected error message.

--- calc.go
+++ calc.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 )
 
+var DividingErr = errors.New(`invalid value.dividing number can not be "0"`)
+
 func Add(x, y float64) (float64, error) {
 	return x + y, nil
 }
@@ -17,7 +19,7 @@ func Mult(x, y float64) (float64, error) {
 }
 func Div(x, y float64) (float64, error) {
 	if y == 0 {
-		return 0, errors.New(`invalid value.dividing number can not be "0"`)
+		return 0, DividingErr
 	}
 	return x / y, nil
 }
